8d765fbdbd2025c1e63f4af2d5f01ae586e552f6
Steven Thomason <thomason84@hotmail.com>
2019-08-29 13:07:10 -0400

    debugging routing

--- server.js
+++ server.js
@@ -11,3 +11,4 @@ app.get(/.*/, function(req, res) {
 });
 app.listen(port);
 console.log('server started '+ port);
+
--- src/main.js
+++ src/main.js
@@ -1,24 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
-
-import About from './views/About.vue'
-import Home from './views/Home.vue'
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import router from './router'
 
 Vue.config.productionTip = false
 
-const routes = [
-  { path: 'home', name: 'Home', component: Home },
-  { path: 'about', name: 'About', component: About },
-];
-
-const router = new VueRouter({ mode: 'history', routes: routes });
-
 new Vue({
-  render: h => h(App),
-  router
+  router,
+  render: h => h(App)
 }).$mount('#app')
