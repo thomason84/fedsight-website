f9a6246e5be055b6372401602fb40004b324ec7a
Steven Thomason <steven.thomason@fedsight.com>
2019-08-30 12:17:21 -0400

    created hero section and first paragraph

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="nav" class='container-fluid fedsightBlueBackground'>
+      <img alt='Fedsight Logo' src='./assets/fedsight_consult_logo-white.svg' class='logo'>
+      <div style='margin-left: auto;'>
+        <router-link to="/" class='col-md-2 col-xs-3'>Home</router-link> |
+        <router-link to="/about" class='col-md-2 col-xs-3'>About</router-link>
+      </div>
     </div>
     <router-view/>
   </div>
@@ -17,15 +20,27 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  position: fixed;
+  top: 0rem;
+  opacity: 0.8;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #fff;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #F7D72A;
+}
+
+.logo{
+    width: 25rem;
+}
+.st0 {
+    fill: #fff;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <section class='heroSection d-flex align-items-center justify-content-center container-fluid'>
+      <div class='row'>
+        <div class='container'>
+          <div class='row'>
+            <h1 class='slogan col-md-12'>Identify What Matters</h1>
+            <div class='mainCTA fedsightYellowBackground m-auto d-flex align-items-center justify-content-center'>Let Us Show You
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section style='height: 20vh;'>
+      <div class='row'>
+        <div class='container'>
+          <h2 class='fedsightGreen'>We Assess Problems and Tailor Solutions for Federal Agencies</h2>
+          <p>As an insight based consulting firm, we are acutely aware of the admission and programmatic nuances of government agencies.  This is why our approach results in sophisticated outcomes.
+          </p>
+        </div>
+      </div>
+    </section>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -16,3 +35,35 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .home{
+    height: 100vh;
+  }
+  .heroSection{
+    height: 80vh;
+  }
+  .slogan{
+    font-weight:normal;
+    font-style:normal;
+    font-size: 3.5vw;
+    max-width: 100%;
+  }
+  .mainCTA{
+    height: 5rem;
+    width: 22rem;
+    font-size: 2rem;
+  }
+
+  /****************  Media Queries *****************/
+  @media only screen and (max-width: 60rem) {
+  .slogan {
+      font-size: 3rem;
+    }
+  }
+  @media only screen and (min-width: 150rem) {
+  .slogan {
+      font-size: 9rem;
+    }
+  }
+</style>
